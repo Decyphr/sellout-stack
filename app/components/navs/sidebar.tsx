5c6cf9a8a2e708de183eea82c90c01ff413dd80d
blakehartman <bhartman@clickrain.com>
2023-05-03 09:17:46 -0600

    created initial admin layout

---
+++ app/components/navs/sidebar.tsx
@@ -0,0 +1,13 @@
+const Sidebar = () => {
+  return (
+    <div className="fixed w-[240px] h-screen bg-white border-r border-black">
+      <nav>
+        <ul>
+          <li>Link</li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Sidebar;
--- app/routes/admin.tsx
+++ app/routes/admin.tsx
@@ -1,9 +1,20 @@
 import { Outlet } from "@remix-run/react";
+import Menu from "~/components/navs/menu";
 
-export default function () {
+export default function AdminLayout() {
   return (
-    <div>
-      <Outlet />
+    <div className="flex flex-col h-screen">
+      <div>
+        <h1>Remix Content Layer</h1>
+      </div>
+      <header className="fixed top-0 left-0 right-0">
+        <Menu />
+      </header>
+      <main className="flex flex-1 pt-10">
+        <div className="flex flex-1 p-4 md:p-12">
+          <Outlet />
+        </div>
+      </main>
     </div>
   );
 }
