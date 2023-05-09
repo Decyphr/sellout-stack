670f3947b058fa804953cb09eb6e0ee4fc9018e2
blakehartman <bhartman@clickrain.com>
2023-05-09 13:08:04 -0500

    updated to use new remix dev server

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -7,8 +7,15 @@ export const meta: V2_MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div>
+    <>
       <Hero />
-    </div>
+      <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+        <div className="px-4 py-6 sm:px-0">
+          <div className="border-4 border-dashed border-zinc-300 rounded-lg h-96">
+            Text
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
--- remix.config.js
+++ remix.config.js
@@ -10,6 +10,7 @@ module.exports = {
     v2_meta: true,
     v2_normalizeFormMethod: true,
     v2_routeConvention: true,
+    unstable_dev: true
   },
   serverModuleFormat: "cjs",
   tailwind: true
